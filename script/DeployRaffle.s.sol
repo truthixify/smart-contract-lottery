cbaff80c85c80c83bded119ec7d2dfe6a393e2c7
truthixify <truthixify@gmail.com>
2025-03-31 20:20:42 +0100

    >>>fmt<<<

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployRaffle is Script {
+    function run() public {}
+
+    function deployContract() public returns (Raffle, HelperConfig) {}
+}
